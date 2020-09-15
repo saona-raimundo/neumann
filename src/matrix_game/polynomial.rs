a92bc4b5176af3d712b9f706dfcf8be2a8ad857f
raimundo saona <rasa200@gmail.com>
2020-09-15 17:20:50 +0200

    Correct implementation of uniform value-positivty for polynomial matrix through an exponential algorithm

--- src/matrix_game/polynomial.rs
+++ src/matrix_game/polynomial.rs
@@ -1,6 +1,7 @@
 use crate::MatrixGame;
 use ndarray::{Axis, Array2};
 use std::fmt;
+use itertools::Itertools;
 
 /// Polynomial matrix games are [Matrix Games](https://en.wikipedia.org/wiki/Zero-sum_game) whith  
 /// perturbation terms in terms of polynomials.
@@ -34,9 +35,9 @@ impl PolyMatrixGame {
             .max()
             .unwrap() as f64;
         let k: f64 = (self.poly_matrix.len() - 1) as f64;
-        let epsilon_0 = (2. * m * k).powf(-m * k)
+        let epsilon_0 = f64::min(1.0, (2. * m * k).powf(-m * k)
             * (b * m).powf(m * (1. - 2. * m * k))
-            * (m * k + 1.).powf(1. - 2. * m * k);
+            * (m * k + 1.).powf(1. - 2. * m * k));
 
         self.eval(epsilon_0).value() >= self.eval(0.).value()
     }
@@ -51,12 +52,17 @@ impl PolyMatrixGame {
     /// in the perturbed matrix game is at least as much as
     /// the value of the matrix game corresponding to evaluating the polynomial matrix game at zero.
     pub fn is_uniform_value_positive(&self) -> bool {
-    	if self.degree() == 1 {
-    		self.linear_is_uniform_value_positive()
+    	if self.is_value_positive() {
+        	if self.degree() == 1 {
+	    		self.linear_is_uniform_value_positive()
+	    	} else {
+	    		self.poly_is_uniform_value_positive()
+	    	}		
     	} else {
-    		unimplemented!("Only implemented for polynomial matrix games which are linear.");
+    		false
     	}
-    }
+
+	    }
 
     fn linear_is_uniform_value_positive(&self) -> bool {
         // Setting
@@ -88,13 +94,12 @@ impl PolyMatrixGame {
         let error_free_value = self.eval(0.0).value();
         for column_action in 0..dimensions[1] {
             let rewards = self.poly_matrix[0].index_axis(Axis(1), column_action).map(|x| *x as f64);
-            let mut constrain = row_strategy
+            let constrain = row_strategy
                 .clone()
                 .into_iter()
                 .zip(rewards.into_iter().cloned())
                 .collect::<Vec<(minilp::Variable, f64)>>();
-            constrain.push((value_variable, -1.0));
-            problem.add_constraint(constrain, minilp::ComparisonOp::Eq, error_free_value);
+            problem.add_constraint(constrain, minilp::ComparisonOp::Ge, error_free_value);
         }
 
         // Value constrains: positive in the second matrix
@@ -109,7 +114,6 @@ impl PolyMatrixGame {
             problem.add_constraint(constrain, minilp::ComparisonOp::Ge, 0.0);
         }
 
-
         // Solve
         match problem.solve() {
         	Ok(solution) => solution.objective() >= 0.0,
@@ -117,95 +121,97 @@ impl PolyMatrixGame {
         }
     }
 
-	// fn old_is_uniform_value_positive(&self) -> bool {
-        // // Computing epsilon_0
-        // let m: f64 = *self.poly_matrix[0].shape().iter().max().unwrap() as f64;
-        // let b: f64 = self
-        //     .poly_matrix
-        //     .iter()
-        //     .map(|matrix| matrix.iter().map(|x| x.abs()).max().unwrap())
-        //     .max()
-        //     .unwrap() as f64;
-        // let k: f64 = (self.poly_matrix.len() - 1) as f64;
-        // let epsilon_1 = 1. / (2. * m * k * b);
-        // let lower_bound = 1. / (b.powf(m) * m.powf(m / 2.));
+    /// # Remarks
+    /// 
+    /// The implementation could be optimize by deducing that some problems need not to be checked given previous results. 
+    /// For now, all exponentially-many problems are checked.
+    fn poly_is_uniform_value_positive(&self) -> bool {
+        // Setting
+        let dimensions = self.poly_matrix[0].shape();
 
-        // let mut null_actions = Vec::new(); // I_0
-        // let mut positive_actions = Vec::new(); // I_1
-        // let error_free_value = self.eval(0.0).value();
-        // for i in 0..(self.poly_matrix[0].shape()[0]) { // All actions of the row player
-        // 	let mut extended_positive_actions = positive_actions.clone();
-        // 	extended_positive_actions.extend(0..(self.poly_matrix[0].shape()[0]));
-        // 	let problem = self.restricted_lp(epsilon_1, lower_bound, &null_actions, &extended_positive_actions);
-        // 	let solution = problem.solve().unwrap();
-        // 	if solution.objective() >= error_free_value {
-        // 		println!("The polynomial matrix game:\n{}", self);
-        // 		println!("is uniform value-positive by: {:?}", solution.iter().map(|(_, p)| *p).collect::<Vec<f64>>());
-        // 		return true
-        // 	}
-        // 	null_actions.push(i);
-        // 	let problem = self.restricted_lp(epsilon_1, lower_bound, &null_actions, &positive_actions);
-        // 	if !(problem.solve().unwrap().objective() >= error_free_value) {
-        // 		null_actions.pop();
-        // 		positive_actions.push(i);
-        // 	}
-        // }
+        // Add extra matrix of ones
+        let mut poly_matrix = self.poly_matrix.clone();
+        poly_matrix.push(Array2::from_elem(self.poly_matrix[0].raw_dim(), 1));
+        let augmented_matrix_game = PolyMatrixGame::from(poly_matrix);
 
-        // false
-    // }
+        // Define LP basis
+        let mut problem = minilp::Problem::new(minilp::OptimizationDirection::Maximize);
+        // Add row player strategy
+        let mut row_strategy = Vec::with_capacity(dimensions[0]);
+        for _ in 0..dimensions[0] {
+            row_strategy.push(problem.add_var(0.0, (0.0, 1.0)));
+        }
+        // Add value variable
+        let value_variable = problem.add_var(1.0, (std::f64::NEG_INFINITY, std::f64::INFINITY));
 
-    // /// Returns an LP where possible actions are restricted.
-    // fn restricted_lp(&self, epsilon: f64, lower_bound: f64, null_actions: &Vec<usize>, positive_actions: &Vec<usize>) -> minilp::Problem {
-    //     // Define LP
-    //     let mut problem = minilp::Problem::new(minilp::OptimizationDirection::Maximize);
+        // Probabiltiy constrains
+        let mut ones = Vec::with_capacity(row_strategy.len());
+        for _ in 0..row_strategy.len() {
+            ones.push(1.0);
+        }
+        problem.add_constraint(
+            row_strategy.clone().into_iter().zip(ones),
+            minilp::ComparisonOp::Eq,
+            1.0,
+        );
 
-    //     // Setting
-    //     let matrix = self.eval(epsilon).matrix().clone();
-    //     let dimensions = matrix.shape();
+        // Value constrains: optimal solution of the first matrix
+        let error_free_value = augmented_matrix_game.eval(0.0).value();
+        for column_action in 0..dimensions[1] {
+            let rewards = augmented_matrix_game.poly_matrix[0].index_axis(Axis(1), column_action).map(|x| *x as f64);
+            let constrain = row_strategy
+                .clone()
+                .into_iter()
+                .zip(rewards.into_iter().cloned())
+                .collect::<Vec<(minilp::Variable, f64)>>();
+            problem.add_constraint(constrain, minilp::ComparisonOp::Ge, error_free_value);
+        }
 
-    //     // Add row player strategy
-    //     let mut row_strategy = Vec::with_capacity(dimensions[0]);
-    //     for _ in 0..dimensions[0] {
-    //         row_strategy.push(problem.add_var(0.0, (0.0, 1.0)));
-    //     }
-    //     // Add value variable
-    //     let value_variable = problem.add_var(1.0, (-std::f64::INFINITY, std::f64::INFINITY));
+        // Iterate over exponentially-many LPs 
+        let index_vectors = (0..dimensions[1]).map(|_| 1..=augmented_matrix_game.degree())
+        	.multi_cartesian_product();
 
-    //     // Probabiltiy constrains
-    //     let mut ones = Vec::with_capacity(row_strategy.len());
-    //     for _ in 0..row_strategy.len() {
-    //         ones.push(1.0);
-    //     }
-    //     problem.add_constraint(
-    //         row_strategy.clone().into_iter().zip(ones),
-    //         minilp::ComparisonOp::Eq,
-    //         1.0,
-    //     );
+        for index_vector in index_vectors {
+        	// Define the LP
+        	let mut index_problem = problem.clone();
+        	
+	        // Value constrains: positive before the index_vactor
+	        for column_action in 0..dimensions[1] {
+		        for matrix_index in 1..index_vector[column_action] {
+		            let rewards = augmented_matrix_game.poly_matrix[matrix_index].index_axis(Axis(1), column_action).map(|x| *x as f64);
+		            let constrain = row_strategy
+		                .clone()
+		                .into_iter()
+		                .zip(rewards.into_iter().cloned())
+		                .collect::<Vec<(minilp::Variable, f64)>>();
+		            index_problem.add_constraint(constrain, minilp::ComparisonOp::Eq, 0.0);
+		        }
+		    }
 
-    //     // Value constrains
-    //     for column_action in 0..dimensions[1] {
-    //         let rewards = matrix.index_axis(Axis(1), column_action);
-    //         let mut constrain = row_strategy
-    //             .clone()
-    //             .into_iter()
-    //             .zip(rewards.into_iter().cloned())
-    //             .collect::<Vec<(minilp::Variable, f64)>>();
-    //         constrain.push((value_variable, -1.0));
-    //         problem.add_constraint(constrain, minilp::ComparisonOp::Ge, 0.0);
-    //     }
+		    // Value constrains: maximizing the last index
+		    for column_action in 0..dimensions[1] {
+		    	let matrix_index = index_vector[column_action];
+		    	println!("{:?}", augmented_matrix_game.poly_matrix[matrix_index]);
+	            let rewards = augmented_matrix_game.poly_matrix[matrix_index].index_axis(Axis(1), column_action).map(|x| *x as f64);
+	            let mut constrain = row_strategy
+	                .clone()
+	                .into_iter()
+	                .zip(rewards.into_iter().cloned())
+	                .collect::<Vec<(minilp::Variable, f64)>>();
+	            constrain.push((value_variable, -1.0));
+	            index_problem.add_constraint(constrain, minilp::ComparisonOp::Ge, 0.0);
+	        }
 
-    //     // Null restrictions
-    //     for i in null_actions {
-    //     	problem.add_constraint(&[(row_strategy[*i], 1.0)], minilp::ComparisonOp::Eq, 0.0);
-    //     }
+        	if let Ok(solution) = index_problem.solve() {
+        		if solution.objective() > 0.0 {
+        			return true
+        		};
+        	}
+        }
 
-    //     // Positive restrictions
-    //     for i in positive_actions {
-    //     	problem.add_constraint(&[(row_strategy[*i], 1.0)], minilp::ComparisonOp::Ge, lower_bound);
-    //     }
+        false
 
-    //     problem
-    // }
+    }
 }
 
 impl<T> From<Vec<Array2<T>>> for PolyMatrixGame 
@@ -236,11 +242,14 @@ impl fmt::Display for PolyMatrixGame {
         let mut string = String::new();
         for i in 0..self.poly_matrix.len() {
             string += &format!("{}", self.poly_matrix[i]);
+            if i == 1 {
+            	string += "eps";
+            }
+            if i > 1 {
+                string += &format!("eps^{}", i);
+            }
             if i < self.poly_matrix.len() - 1 {
                 string += "\n+\n"
-            }
-            if i > 0 {
-                string += &format!("eps^{}", i);
             }
         }
         write!(f, "{}", string)
@@ -278,7 +287,9 @@ mod tests {
     #[test_case( vec![ array![[0, 0], [-1, 1]], array![[2, -1], [0, 0]] ],  false ; "not uniform value-positive medium")]
     #[test_case( vec![ array![[1, -1], [-1, 1]], array![[1, -3], [0, 2]] ],  false ; "not uniform value-positive hard")]
     #[test_case( vec![ array![[0, 1], [1, 0]], array![[0, -1], [-1, 0]] ],  false ; "not uniform value-positive easy")]
-    #[test_case( vec![ array![[0]], array![[0]], array![[0]] ], true => panics "Only implemented for polynomial matrix games which are linear.")]
+    #[test_case( vec![ array![[0]], array![[0]], array![[0]] ], true ; "uniform value-positive easy polynomial")]
+    #[test_case( vec![ array![[1, -1], [-1, 1]], array![[2, -2], [-2, 2]], array![[3, -3], [-3, 3]] ], true ; "uniform value-positive medium polynomial")]
+    #[test_case( vec![ array![[1, 1], [1, 1]], array![[2, -1], [-1, 2]], array![[2, -1], [-1, 2]] ], true ; "uniform value-positive medium-hard polynomial")]
     fn computing_uniform_value_positivity(poly_matrix: Vec<Array2<i32>>, expected_value: bool) {
         let poly_matrix_game = PolyMatrixGame::from(poly_matrix);
         assert_eq!(poly_matrix_game.is_uniform_value_positive(), expected_value);
